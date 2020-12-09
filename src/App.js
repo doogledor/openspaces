d711b30a3dfcb6dfdb58c3ddf3800bb2caed7d12
Sam Elie <samradelie@gmail.com>
2020-12-08 17:16:31 -0800

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-2">
+        part of the Container for #2 with overflow
+        <Anchor>ANCHOR</Anchor>
+      </div>
+      <div className="container-3">
+        part of the Container for #3
+        <Anchor>ANCHOR</Anchor>
+        <Anchor>ANCHOR item 2</Anchor>
+        <Anchor>ANCHOR item 3</Anchor>
+        <Anchor>ANCHOR item 4</Anchor>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+/* --------------------
+managed by the user, separte from tooltip
+ ----------------------*/
+function Anchor(props) {
+  const contentRef = useRef(null)
+
+  return (
+    <div ref={contentRef} className="anchor">
+      <div className="content">{props.children}</div>
+      <Toolip className="toolip" targetRef={contentRef}>
+        TOOLIP
+      </Toolip>
+    </div>
+  )
+}
+/* --------------------
+
+ ----------------------*/
+function Toolip({ targetRef, children }) {
+  const tooltipRef = useRef(null)
+  const [state, setState] = useState({
+    style: { left: 0, top: 0, visibility: 'hidden' },
+  })
+  const [visibility, setVisibility] = useState({
+    visible: false,
+  })
+
+  useEffect(() => {
+    if (targetRef.current) {
+      targetRef.current.addEventListener('mouseover', mouseOver)
+      targetRef.current.addEventListener('mouseout', mouseOut)
+      targetRef.current.addEventListener('mouseleave', mouseOut)
+      targetRef.current.addEventListener('wheel', onWheel)
+    }
+    return () => {
+      if (targetRef.current) {
+        targetRef.current.removeEventListener('mouseover', mouseOver)
+        targetRef.current.removeEventListener('mouseout', mouseOut)
+        targetRef.current.removeEventListener('mouseleave', mouseOut)
+        targetRef.current.removeEventListener('wheel', onWheel)
+      }
+    }
+  }, [targetRef])
+
+  useEffect(() => {
+    if (!visibility.visible || !tooltipRef.current) return
+    const tooltipRect = tooltipRef.current.getBoundingClientRect()
+    const targetBbox = targetRef.current.getBoundingClientRect()
+    setState({
+      style: {
+        left: `${Math.floor(targetBbox.x + targetBbox.width + 10)}px`,
+        top: `${Math.floor(
+          targetBbox.y + targetBbox.height / 2 - tooltipRect.height / 2,
+        )}px`,
+        visibility: 'visible',
+      },
+    })
+  }, [visibility])
+
+  function onWheel(e) {
+    setVisibility({ visible: false })
+  }
+
+  function mouseOver(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    setVisibility({ visible: true })
+    return false
+  }
+
+  const mouseOut = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    setVisibility({ visible: false })
+    return false
+  }
+
+  if (!visibility.visible) return null
+
+  return (
+    <div
+      ref={tooltipRef}
+      onMouseOver={mouseOver}
+      onMouseOut={mouseOut}
+      className="toolip"
+      style={state.style}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default App
